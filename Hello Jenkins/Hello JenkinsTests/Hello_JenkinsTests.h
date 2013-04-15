df23db07b7a84f4fe9f3d875367fcee4e1a84a4a
Richard Kenny <Richard.Kenny@compuware.com>
2013-04-15 10:32:38 -0400

    Source
    Adding source code

---
+++ Hello Jenkins/Hello JenkinsTests/Hello_JenkinsTests.h
@@ -0,0 +1,13 @@
+//
+//  Hello_JenkinsTests.h
+//  Hello JenkinsTests
+//
+//  Created by amirxk0 on 4/15/13.
+//  Copyright (c) 2013 compuware. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Hello_JenkinsTests : SenTestCase
+
+@end
---
+++ Hello Jenkins/Hello JenkinsTests/Hello_JenkinsTests.m
@@ -0,0 +1,32 @@
+//
+//  Hello_JenkinsTests.m
+//  Hello JenkinsTests
+//
+//  Created by amirxk0 on 4/15/13.
+//  Copyright (c) 2013 compuware. All rights reserved.
+//
+
+#import "Hello_JenkinsTests.h"
+
+@implementation Hello_JenkinsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Hello JenkinsTests");
+}
+
+@end
